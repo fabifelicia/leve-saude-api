fb475bbbee6dbe8a2888cfd36f18c88658008ad6
fabifelicia <fabianabarretomenezes@gmail.com>
2025-07-19 16:30:28 -0300

    config: ajustes nos arquivos de configuracao

--- jest.config.ts
+++ jest.config.ts
@@ -1,20 +1,26 @@
 import type { Config } from "jest";
 
-const config : Config = {
-  preset: 'ts-jest',
+const config: Config = {
+  preset: "ts-jest",
   testEnvironment: "node",
-  testMatch: ['**/tests/**/*.test.ts'],
-  moduleFileExtensions: ['ts', 'js', 'json'],
-  roots: ['<rootDir>/src', '<rootDir>/tests'],
+  testMatch: ["**/tests/**/*.test.ts"],
+  moduleFileExtensions: ["ts", "js", "json"],
+  roots: ["<rootDir>/src", "<rootDir>/tests"],
   transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
+    "^.+\\.(ts|tsx)$": "ts-jest",
   },
   clearMocks: true,
-  coverageDirectory: 'coverage',
-  collectCoverageFrom: [
-    'src/**/*.{ts}',
-  ],
+  coverageDirectory: "coverage",
+  collectCoverage: true,
+  collectCoverageFrom: ["src/**/*.{ts,js}"],
+  coverageThreshold: {
+    global: {
+      branches: 60,
+      functions: 60,
+      lines: 60,
+      statements: 60,
+    },
+  },
 };
 
 export default config;
-
